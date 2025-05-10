d5cd1b1ce061c25dca9ba8ba6bcea037304eca98
SaiVineeth01 <kasojusaivineeth902@gmail.com>
2025-05-10 20:33:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app import create_app, db  # Import the create_app factory and db
 from dotenv import load_dotenv
 import os
+from app import socketio
 
 # Load environment variables from a .env file
 load_dotenv()
@@ -13,5 +14,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    socketio.run(app)
 
